8d52466f7cce55c705aa666e962cd96a348b724e
LinShaoChing <mormor19971218@gmail.com>
2021-06-15 19:47:44 +0800

    initial mvsnet

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-MVS_TRAINING="/home/xyguo/dataset.ssd/dtu_mvs/processed/mvs_training/dtu/"
-python train.py --dataset=dtu_yao --batch_size=4 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/d192 $@
+MVS_TRAINING="/home/linsc/dataset/mvs_training/dtu/"
+python train.py --dataset=dtu_yao --batch_size=1 --trainpath=$MVS_TRAINING --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/d192 $@
+# python train.py --dataset=dtu_yao  --mode train --batch_size=1 --trainpath=$MVS_TRAINING --trainlist lists/dtu/mini_train.txt --testlist lists/dtu/test.txt --numdepth=192 --logdir ./checkpoints/d192 $@
