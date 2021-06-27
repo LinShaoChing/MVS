9b58c5cb34a98ebb7b89720c0a64e1be1d09206c
LinShaoChing <mormor19971218@gmail.com>
2021-06-27 16:22:17 +0800

    2021 06 27

--- eval.sh
+++ eval.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 DTU_TESTING="/home/linsc/dataset/dtu/"
 # CKPT_FILE="./checkpoints/d192/d=160_ViewNum=3.ckpt"
-CKPT_FILE="pre_trained.ckpt"
+CKPT_FILE="./checkpoints/d192/d=160_ViewNum=5_epoch=8.ckpt"
+# CKPT_FILE="pre_trained.ckpt"
 python eval.py --dataset=dtu_yao_eval --batch_size=1 --testpath=$DTU_TESTING --testlist lists/dtu/mini_test.txt --loadckpt $CKPT_FILE $@
